bda78a8e43d426cd15b79332426cff37f4c484ad
DragonOfDissent <dvpsherman@gmail.com>
2021-12-02 22:58:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/courseselection/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.courseselection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    enum class LoginSuccess
+        constructor(val intValue: Int){
+            login(1),
+            password(2),
+            success(0)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtLogin = findViewById<TextView>(R.id.idLoginTxt)
+        val txtPassword = findViewById<TextView>(R.id.idPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        fun checkLogin(txtLogin: String, txtPassword: String): LoginSuccess{
+            val holdLogin = "David"
+            val holdPass = "password"
+
+            if (holdLogin != txtLogin){
+                return LoginSuccess.login
+            }
+            return if (holdPass != txtPassword){
+                return LoginSuccess.password
+            }else LoginSuccess.success
+        }
+
+        btnLogin.setOnClickListener {
+            when (checkLogin(txtLogin.text.toString(), txtPassword.text.toString())){
+                LoginSuccess.login -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessageLogin), Toast.LENGTH_LONG).show()
+                    txtLogin.requestFocus()
+                }
+
+                LoginSuccess.password -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessagePassword), Toast.LENGTH_LONG).show()
+                    txtPassword.requestFocus()
+                }
+
+                else -> Toast.makeText(applicationContext, "Success", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
+    }
+}
